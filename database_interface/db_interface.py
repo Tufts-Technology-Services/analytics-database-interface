693fccbad543eb9f62eb6767f97b45c8afb7464a
cbarne02 <christopher.barnett@tufts.edu>
2023-02-21 11:44:57 -0500

    add a bit of documentation

--- database_interface/db_interface.py
+++ database_interface/db_interface.py
@@ -5,15 +5,35 @@ from .utils import create_db_engine
 
 
 class DatabaseInterface:
-    def __init__(self, user=None, passwd=None, server='localhost',
+    """
+    Used to handle low level database operations and centralize database code.
+    """
+    def __init__(self, user=None, passwd=None, host='localhost',
                  database='rt_analytics', flavor='mysql', verbose=False, engine=None):
+        """
+        provide database connection string info. alternatively, pass an existing SQLAlchemy engine
+        :param user:
+        :param passwd:
+        :param host:
+        :param database:
+        :param flavor:
+        :param verbose:
+        :param engine:
+        """
         if engine is None:
-            self.engine = create_db_engine(user, passwd, server, database, flavor, verbose=verbose)
+            self.engine = create_db_engine(user, passwd, host, database, flavor, verbose=verbose)
         else:
             self.engine = engine
         self.database = database
 
     def read_df(self, table_name=None, cols=None, sql=None):
+        """
+        read the contents of a table into a dataframe
+        :param table_name:
+        :param cols:
+        :param sql:
+        :return:
+        """
         with self.engine.connect() as conn:
             if sql is not None:
                 return pd.read_sql(sql, con=conn)
@@ -44,30 +64,70 @@ class DatabaseInterface:
         return r.fetchone()[0]
 
     def fetch(self, sql):
+        """
+        fetch all records for the given SQL
+        :param sql:
+        :return:
+        """
         r = self.execute(sql)
         return r.fetchall()
 
     def execute(self, sql):
+        """
+        execute a SQL command
+        :param sql:
+        :return:
+        """
         with self.engine.connect() as conn:
             return conn.execute(text(sql))
 
     def execute_sql_file(self, sql_path, format_strings=None):
+        """
+        execute a SQL command from a text file
+        :param sql_path:
+        :param format_strings:
+        :return:
+        """
         if format_strings is None:
             format_strings = {}
         with open(sql_path) as f:
             return self.execute(f.read().strip().format(**format_strings))
 
     def has_table(self, table_name):
+        """
+        checks whether a given table exists in the database
+        :param table_name:
+        :return:
+        """
         insp = inspect(self.engine)
         return insp.has_table(table_name, schema=self.database)
 
     def append_df(self, dataframe, table_name):
+        """
+        append the contents of a dataframe to the given table
+        :param dataframe:
+        :param table_name:
+        :return:
+        """
         self._write_df(dataframe, table_name, if_exists='append')
 
     def replace_df(self, dataframe, table_name):
+        """
+        replace the contents of a table with the contents of a dataframe
+        :param dataframe:
+        :param table_name:
+        :return:
+        """
         self.execute(f'TRUNCATE TABLE {table_name}')
         self._write_df(dataframe, table_name, if_exists='append')
 
     def _write_df(self, dataframe, table_name, if_exists=None):
+        """
+        writes a dataframe to a table
+        :param dataframe:
+        :param table_name:
+        :param if_exists:
+        :return:
+        """
         with self.engine.connect() as conn:
             dataframe.to_sql(table_name, conn, if_exists=if_exists, index=False)
--- database_interface/db_writer.py
+++ database_interface/db_writer.py
@@ -4,13 +4,23 @@ from .utils import create_db_engine
 
 
 class DBWriter:
-    def __init__(self, user=None, passwd=None, db='rt_analytics', host='localhost', flavor='mysql', verbose=False):
-        user = urllib.parse.quote(user)
-        passwd = urllib.parse.quote(passwd)
-
-        self.engine = create_db_engine(user, passwd, host, db, flavor, verbose)
+    """
+    parent class for writing data rows to a database table.
+    """
+    def __init__(self, user=None, passwd=None, database='rt_analytics', host='localhost',
+                 flavor='mysql', verbose=False, engine=None):
+        if engine is None:
+            self.engine = create_db_engine(user, passwd, host, database, flavor, verbose=verbose)
+        else:
+            self.engine = engine
 
     def persist(self, rows, batch_size=50):
+        """
+        write a list of data rows to a table
+        :param rows:
+        :param batch_size:
+        :return:
+        """
         for i in range(0, len(rows), batch_size):
             with Session(self.engine) as session, session.begin():
                 try:
@@ -21,5 +31,12 @@ class DBWriter:
                     raise e
 
     def transform(self, row, session):
+        """
+        abstract function for transforming a data row from its native form to the format expected by the database.
+        override this.
+        :param row:
+        :param session:
+        :return:
+        """
         pass
 
