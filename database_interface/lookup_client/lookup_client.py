fc6244233b0b87b90bea7429d31c85eb7253eb3c
Chris Barnett <Christopher.Barnett@tufts.edu>
2024-10-07 12:47:04 -0400

    fix references

--- database_interface/lookup_client/lookup_client.py
+++ database_interface/lookup_client/lookup_client.py
@@ -1,5 +1,5 @@
 from database_interface import DatabaseInterface
-from .errors import InvalidEmailAddressError, InvalidCourseCatalogNumberError
+from .errors import InvalidEmailAddressError, InvalidCourseCatalogNumberError, APIError, NoMatchFoundError
 
 
 class LookupClient:
@@ -10,104 +10,39 @@ class LookupClient:
         self.dbi = database_interface        
 
     def get_email(self, utln):
-        return self.dbi.fetch(f"SELECT pr_identity_email as email FROM pr_fis_vw WHERE pr_identity_utln = '{utln}'")
+        r = self.dbi.fetch(f"SELECT pr_identity_email FROM pr_fis_vw WHERE pr_identity_utln = '{utln}'")
+        if len(r) == 0:
+            raise NoMatchFoundError(f'No match found for utln {utln}')
+        return r[0][0]
 
     def utln_lookup(self, utln):
-        """
-        def clean_strings(df, col_name):
-    df[col_name] = df[col_name].str.replace('-', ' - ')
-    df[col_name] = df[col_name].str.replace(r'\s+', ' ', regex=True)
-    df[col_name] = df[col_name].str.replace('  ', ' ')
-    df[col_name] = df[col_name].str.replace('_', ' ').str.strip()
-    return df
+        fields = [{"src": "pr_identity_utln", "tgt": "utln", "type": "keyword"}, 
+                  {"src": "pr_identity_email", "tgt": "email", "type": "keyword"},
+                  {"src": "pr_identity_firstname", "tgt": "first_name", "type": "keyword"},
+                  {"src": "pr_identity_lastname", "tgt": "last_name", "type": "keyword"},
+                  {"src": "user_primary_affiliation", "tgt": "primary_affiliation", "type": "keyword"},
+                  {"src": "user_primary_title", "tgt": "primary_title", "type": "keyword"},
+                  {"src": "user_primary_division", "tgt": "division", "type": "keyword"},
+                  {"src": "user_primary_dept_prog", "tgt": "dept_prog", "type": "keyword"},
+                  {"src": "user_primary_campus", "tgt": "campus", "type": "keyword"},
+                  {"src": "user_profile_link", "tgt": "profile_link", "type": "keyword"},
+                  {"src": "user_pr_status", "tgt": "is_active", "type": "boolean"}]
 
-    "filter": ["lowercase", "asciifolding"]
-        [
-  {
-    "src_name": "pr_identity_utln",
-    "tgt_name": "utln",
-    "tgt_type": "keyword"
-  },
-  {
-    "src_name": "pr_identity_prid",
-    "tgt_name": "pr_id",
-    "tgt_type": "keyword"
-  },
-  {
-    "src_name": "fis_proprietary_id",
-    "tgt_name": "fis_id",
-    "tgt_type": "keyword"
-  },
-  {
-    "src_name": "pr_identity_email",
-    "tgt_name": "email",
-    "tgt_type": "keyword",
-    "normalizer": "std_normalizer"
-  },
-  {
-    "src_name": "pr_identity_firstname",
-    "tgt_name": "first_name",
-    "tgt_type": "keyword",
-    "normalizer": "std_normalizer"
-  },
-  {
-    "src_name": "pr_identity_middlename",
-    "tgt_name": "middle_name",
-    "tgt_type": "keyword"
-  },
-  {
-    "src_name": "pr_identity_lastname",
-    "tgt_name": "last_name",
-    "tgt_type": "keyword",
-    "normalizer": "std_normalizer"
-  },
-  {
-    "src_name": "user_primary_affiliation",
-    "tgt_name": "primary_affiliation",
-    "tgt_type": "keyword",
-    "normalizer": "std_normalizer"
-  },
-  {
-    "src_name": "user_primary_title",
-    "tgt_name": "primary_title",
-    "tgt_type": "keyword",
-    "normalizer": "std_normalizer"
-  },
-  {
-    "src_name": "user_primary_division",
-    "tgt_name": "division",
-    "tgt_type": "keyword",
-    "normalizer": "std_normalizer"
-  },
-  {
-    "src_name": "user_primary_dept_prog",
-    "tgt_name": "dept_prog",
-    "tgt_type": "keyword",
-    "normalizer": "std_normalizer"
-  },
-  {
-    "src_name": "user_primary_campus",
-    "tgt_name": "campus",
-    "tgt_type": "keyword",
-    "normalizer": "std_normalizer"
-  },
-  {
-    "src_name": "user_profile_link",
-    "tgt_name": "profile_link",
-    "tgt_type": "keyword"
-  },
-  {
-    "src_name": "user_pr_status",
-    "tgt_name": "is_active",
-    "tgt_type": "boolean"
-  }
-]"""
-        return self.dbi.fetch(f"SELECT pr_identity_utln as utln, pr_identity_email as email FROM pr_fis_vw WHERE pr_identity_utln = '{utln}'")
-
+        sql = f"""
+          SELECT pr_identity_utln, pr_identity_email, pr_identity_firstname, pr_identity_lastname, 
+          user_primary_affiliation, pr_hr_title, pr_med_title, user_primary_division, user_primary_dept_prog, 
+          user_primary_campus, user_profile_link, user_pr_status FROM pr_fis WHERE pr_identity_utln = '{utln}'
+          """
+        r = self.dbi.fetch(sql)
+        if len(r) == 0:
+            raise NoMatchFoundError(f'No match found for utln {utln}')
+        return r[0]
 
     def email_lookup(self, email):
-        return self.dbi.fetch(f"SELECT pr_identity_utln as utln, pr_identity_email as email FROM pr_fis_vw WHERE pr_identity_email = '{email}'")
-
+        r = self.dbi.fetch(f"SELECT pr_identity_utln as utln, pr_identity_email as email FROM pr_fis_vw WHERE pr_identity_email = '{email}'")
+        if len(r) == 0:
+            raise NoMatchFoundError(f'No match found for email {email}')
+        return r[0][0]
 
     def course_lookup(self, course_num):
         """
@@ -156,17 +91,13 @@ class LookupClient:
   }
 ]
         """
-        return self.dbi.fetch(f"SELECT pr_identity_utln as utln, pr_identity_email as email FROM pr_fis_vw WHERE pr_identity_email = '{email}'")
-
-    
-    def get_tag_from_techconnect(self, tag):
-        pass
+        return self.dbi.fetch(f"SELECT pr_identity_utln as utln, pr_identity_email as email FROM pr_fis_vw WHERE pr_identity_email = '{course_num}'")
 
     def jira_tag_from_mapping(self, tag):
         pass
 
     def get_tag_mappings(self):
-        pass
+        return []
 
     def get_tag_from_techconnect(self, tag):
         tags = self.get_tag_mappings()
@@ -176,7 +107,7 @@ class LookupClient:
         elif len(match) == 1:
             return match[0]
         else:
-            raise Exception('too many matches in tag set.. please review')
+            raise APIError('too many matches in tag set.. please review')
 
 
 
